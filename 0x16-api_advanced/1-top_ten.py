acd3bed2dd8912d083135222493fcf8cd8b50aa4
Dit123 <debhaitulua@gmail.com>
2023-10-10 10:59:21 -0700

    all

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
-"""prints the titles of the first 10 hot posts listed for a given subreddit
-"""
+
 import requests
 
-
 def top_ten(subreddit):
-    """prints the titles of the first 10 hot posts listed for a given subreddit
-    """
-    if subreddit is None:
-        return 0
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
-                Gecko/20100101 Firefox/108.0"
-    }
-    params = {
-            "limit": 10
-            }
-    response = requests.get(url, params=params, headers=headers,
-                            allow_redirects=False).json()
-    children = response.get("data", {}).get("children", None)
+    # Define the Reddit API URL for the subreddit's hot posts
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    if children:
-        for topic in children:
-            print(topic.get("data").get("title"))
-    else:
-        print("None")
+    # Set a custom User-Agent to avoid potential issues
+    headers = {'User-Agent': 'MyBot/1.0'}
+
+    try:
+        # Send an HTTP GET request to the API
+        response = requests.get(url, headers=headers)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            # Extract and print the titles of the first 10 hot posts
+            for post in data['data']['children']:
+                print(post['data']['title'])
+
+        # If the subreddit is invalid, Reddit may return a redirect (status code 302)
+        # In that case, print None as specified in the requirements
+        elif response.status_code == 302:
+            print("None")
+
+        else:
+            # Handle other possible errors here, such as 404 (Not Found)
+            print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        # Handle network-related errors here
+        print(f"An error occurred: {e}")
+
+# Example usage:
+subreddit_name = "learnpython"
+print(f"Top 10 hot posts in subreddit: {subreddit_name}")
+top_ten(subreddit_name)
+
